bac89e7278ea6fba35c27414b67a2db496c552cd
Gabriel Ferreira <you@example.com>
2020-05-22 12:27:31 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace PrimeiroPOO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+               personagem Batman = new personagem();
+            Batman.armadura = "Hellbat";
+
+            Console.WriteLine(Batman.nome);
+
+            Console.WriteLine(Batman.armadura);
+
+            Console.WriteLine(Batman.Atacar());
+        }
+    }
+}
---
+++ personagem.cs
@@ -0,0 +1,17 @@
+namespace PrimeiroPOO
+{
+    public class personagem
+    {
+        public string nome = "Batman";
+        public int idade = 40;
+        public string armadura;
+
+
+        public string Atacar()
+        {
+            return "O personagem atacou!";
+        }
+
+
+    }
+}
